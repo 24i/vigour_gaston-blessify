2817724d072eec8f9b5f669beaa738e4a8698e23
André Padez <andre.padez@gmail.com>
2015-09-30 14:49:36 +0200

    now accounting for ~

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
 var path = require('path')
-  , less = require('less')
-  , through = require('through2')
-  , CSS_EXTENSIONS = ['.css', '.less']
-  , JS_EXTENSIONS = ['.js', '.json']
-  , importRegExp = /^(\s+)?(?!\s*\/\/\s*)@import(\ +)?url(\ +)?\((\ +)?['"](.+)?['"](\ +)?\)\;?/gm
-  , requireRegExp = /^(\s+)?(?!\s*\/\/\s*)require(\ +)?\((\ +)?['"](.+)?['"](\ +)?\)\;?/gm
-  , tildeRegExp = new RegExp('^\~\/')
-  , appRegExp
-  , options
+var less = require('less')
+var through = require('through2')
+var CSS_EXTENSIONS = ['.css', '.less']
+var importRegExp = /^(\s+)?(?!\s*\/\/\s*)@import(\ +)?url(\ +)?\((\ +)?['"](.+)?['"](\ +)?\)\;?/gm
+var tildeRegExp = new RegExp('^\~')
 
+var Blessify = module.exports = function (app) {
+  this.app = app || {}
+  this.app.project = app.project || {}
+  this.app.project.gaston = app.project.gaston || {}
+  this.options = app.project.gaston.lessOptions
+  this.files = {}
+  this.lessCode = ''
+  this.originalCode = ''
+  this.transform = transform(this)
+}
 
-var Blessify = module.exports = function(options){
-  this.options = options;
-  this.files = {};
-  this.lessCode = '';
-  this.originalCode = '';
-  this.transform = transform(this);
-};
-
-Blessify.prototype.render = function(){
-  var self = this;
-  self.lessCode = '';
-  var files = Object.keys(self.files);
-  for(var i = files.length - 1; i >= 0; i--){
-    var file = files[i];
-    var str = self.files[file];
-    self.lessCode += '/* file: ' + file + ' */\n';
-    self.originalCode += '/* file: ' + file + ' */\n';
-    self.originalCode += str + '\n';
-    var lessCode = str.replace(importRegExp, '');
-    self.lessCode += lessCode += '\n';
+Blessify.prototype.render = function () {
+  var self = this
+  self.lessCode = ''
+  var files = Object.keys(self.files)
+  for (var i = files.length - 1; i >= 0; i--) {
+    var file = files[i]
+    var str = self.files[file]
+    self.lessCode += '/* file: ' + file + ' */\n'
+    self.originalCode += '/* file: ' + file + ' */\n'
+    self.originalCode += str + '\n'
+    var lessCode = str.replace(importRegExp, '')
+    self.lessCode += lessCode += '\n'
   }
-  return less.render(self.lessCode, self.options);
-};
+  return less.render(self.lessCode, self.options)
+}
 
-var transform = function(blessify){
-  return function(file, opts){
-    return through(function(buf, enc, next){
-      //if it's not less or css file return original
-      var ext = path.extname(file);
-      if( !~CSS_EXTENSIONS.indexOf(ext) ){
-        return ( this.push(buf) && next() );
-      }
-      
-      var str = buf.toString('utf8');
-
-      var output = '';
-      while( match = importRegExp.exec(str) ){
-        output += 'require(\'' + match[5] + '\');\n';
+var transform = function (blessify) {
+  return function (file, opts) {
+    return through(function (buf, enc, next) {
+      // if it's not less or css file return original
+      var ext = path.extname(file)
+      if (!~CSS_EXTENSIONS.indexOf(ext)) {
+        return (this.push(buf) && next())
       }
 
-      blessify.files[file] = str;
+      var str = buf.toString('utf8')
 
-      this.push(output);
-      return next();
-    });
-  };
-};
+      var output = ''
+      var match = importRegExp.exec(str)
+      while (match) {
+        var importPath = match[5].replace(tildeRegExp, blessify.app.project['base-path'])
+        output += "require('" + importPath + "');\n"
+        match = importRegExp.exec(str)
+      }
+
+      blessify.files[file] = str
+
+      this.push(output)
+      return next()
+    })
+  }
+}
