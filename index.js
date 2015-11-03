8f0a1333e042d49a70f53b65cfdce4fc6c486b29
André Padez <andre.padez@gmail.com>
2015-11-03 12:36:41 +0100

    now allows for external @imports

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ var through = require('through2')
 var CSS_EXTENSIONS = ['.css', '.less']
 var importRegExp = /^(\s+)?(?!\s*\/\/\s*)@import(\ +)?url(\ +)?\((\ +)?['"](.+)?['"](\ +)?\)\;?/gm
 var tildeRegExp = new RegExp('^\~')
-
+console.log('blessify linked')
 var Blessify = module.exports = function (app) {
   this.app = app || {}
   this.app.project = app.project || {}
   this.options = app.gaston['less-options']
   this.files = {}
   this.lessCode = ''
+  this.imports = ''
   this.originalCode = ''
   this.transform = transform(this)
 }
 
 Blessify.prototype.render = function () {
   var self = this
-  self.lessCode = ''
+  self.lessCode = self.imports
   var files = Object.keys(self.files)
   for (var i = files.length - 1; i >= 0; i--) {
     var file = files[i]
@@ -46,7 +47,11 @@ var transform = function (blessify) {
       var match = importRegExp.exec(str)
       while (match) {
         var importPath = match[5].replace(tildeRegExp, blessify.app.project['base-path'])
-        output += "require('" + importPath + "');\n"
+        if(importPath.indexOf('http') === 0 || importPath.indexOf('//') === 0){
+          blessify.imports += match[0] + '\n'
+        } else {
+          output += "require('" + importPath + "');\n"
+        }
         match = importRegExp.exec(str)
       }
 
